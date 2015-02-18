91e7f88a1ba1e56dc683d31c8b92ddf2594a4d7a
13lcp2000 <leo011093@hotmail.com>
2015-02-18 00:03:32 -0500

    script to calculate your continuity in a course

---
+++ script3.py
@@ -0,0 +1,10 @@
+def notaTemp():
+
+	n1 = (int(input("Digite n1")))
+	n2 = (int(input("Digite n2")))
+	n_t = (((n1 + n2)/2)*0.6)
+
+	if ((3 - n_t)/0.4) <= 5 :
+		print "Estudie"
+	else:
+		print "Cancele"
